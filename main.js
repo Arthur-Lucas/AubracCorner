230bbffb780bfe8326941f24f1e192e9f02d3def
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2022-09-21 11:03:06 +0200

    :bug: Overlay height fixed

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+//Menu burger
+const burger = document.querySelector('.menu_burger');
+/*
+if (window.matchMedia("(max-width: 700px)").matches) {
+    burger.onclick
+
+
+
+
+*/
