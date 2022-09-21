a7cc0ccc920985fc1fe1a3f8a753a18ac0392c76
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2022-09-21 12:08:15 +0200

    :sparkles: Add Menu Burger + Anims

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // Scroll change backkground header
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 //Menu burger
-const burger = document.querySelector('.menu_burger');
-/*
-if (window.matchMedia("(max-width: 700px)").matches) {
-    burger.onclick
+const burger = document.querySelector(".menu_burger");
+const burger_icone = document.querySelector("#burger_icone");
+const close_icone = document.querySelector("#close_icone");
 
+burger_icone.onclick = function () {
+    burger.style.transform = "translateX(0)";
+};
+close_icone.onclick = function () {
+    burger.style.transform = "translateX(110vw)";
+    
 
+}
 
-
-*/
